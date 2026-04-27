31b8218d3f7cd2404ae5b60c40581f1b8015dadb
Alan <alan.sandoval5184@alumnos.udg.mx>
2026-04-27 10:13:13 -0600

    validaaores funcionando

--- validador.py
+++ validador.py
@@ -1,4 +1,21 @@
-print("Hola Alan, eres muy feo y agradable")
+def validar_password(password):
+    tiene_mayus = False
+    tiene_signo = False
+
+    signos = "!@#$%&*."
+
+    for c in password:
+        if c.isupper():
+            tiene_mayus = True
+        if c in signos:
+            tiene_signo = True
+
+    if len(password) >= 8 and tiene_mayus and tiene_signo:
+        return True
+    return False
 
 
-print ("Hola Giovanni, pinche proyecto pedorro")
+# Prueba
+print(validar_password("Hola123!"))  # True
+print(validar_password("hola1234"))  # False
+print(validar_password("12113A4."))  # False
