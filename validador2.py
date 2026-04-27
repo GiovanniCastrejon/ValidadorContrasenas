9a8efe021e703f0a17de38278280fc868b1028b2
Giovanni <giovannicastrejon9@gmail.com>
2026-04-27 11:22:00 -0600

    Mejoras en la interfaz

--- validador2.py
+++ validador2.py
@@ -1,4 +1,6 @@
 import re
+import tkinter as tk
+from tkinter import ttk, messagebox, END
 
 regex = r"^(?=.*[A-Z])(?=.*[!@#$%&.*]).{8,}$"
 
@@ -9,3 +11,36 @@ print(validar("Hola123!"))  # True
 print(validar("hola1234"))  # False
 print(validar("12113A4."))
 print(validar("1a2b3@cF"))  # False
+
+root = tk.Tk()
+root.title("Validador de contraseñas")
+root.geometry("600x400")
+icono = tk.PhotoImage(file = "logo.png")
+root.iconphoto(True, icono)
+root.configure(bg = "light grey")
+
+
+titulo = tk.Label(root, text= "Valida que tan segura es tu contraseña")
+titulo.config(bg= "light grey", font= ("Times New Roman", 16, "bold"))
+titulo.place(x = 120, y = 60)
+
+contraseña = tk.Entry(root, bg="white", font=("Arial",11))
+contraseña.place(x=225, y=150)
+
+contraseña.insert(0, "Introduce tu contraseña")
+contraseña.config(fg="gray")
+
+def entrar(event):
+    if contraseña.get() == "Introduce tu contraseña":
+        contraseña.delete(0, END)
+        contraseña.config(fg="black")
+
+def salir(event):
+    if contraseña.get() == "":
+        contraseña.insert(0, "Introduce tu contraseña")
+        contraseña.config(fg="gray", show="")
+
+contraseña.bind("<FocusIn>", entrar)
+contraseña.bind("<FocusOut>", salir)
+
+root.mainloop()
